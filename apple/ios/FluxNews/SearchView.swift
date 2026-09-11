07d7b5342efe2baa25b5563a091c75a42126a1dd
Kevin Fechtel <kevin@kevincfechtel.dev>
2026-09-11 11:40:56 +0200

    Increase search result item spacing

--- apple/ios/FluxNews/SearchView.swift
+++ apple/ios/FluxNews/SearchView.swift
@@ -29,7 +29,7 @@ struct SearchView: View {
             } else {
                 GeometryReader { proxy in
                     let horizontalInset: CGFloat = proxy.size.width > 700 ? 28 : 16
-                    let spacing: CGFloat = ArticlePresentationLayout.usesLandscapeVisual(mode: newsreaderStore.articlePresentationMode, availableWidth: proxy.size.width - horizontalInset * 2) ? 20 : 26
+                    let spacing: CGFloat = 28
                     ScrollView {
                         LazyVStack(spacing: spacing) {
                             ForEach(store.results, id: \.id) { article in
